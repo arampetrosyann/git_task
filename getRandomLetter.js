85696da043983829f1731cfd6ad7101bc662ec49
arampetrosyan <0aramp@gmail.com>
2020-06-28 14:28:17 +0400

    function which returns random letter

---
+++ getRandomLetter.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const getRandomLetter = function () {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+
+    let randChar = '';
+    
+    let random = Math.floor(Math.random() * chars.length);
+           
+    randChar = chars.substring(random, random +1);
+
+    return randChar;
+};
